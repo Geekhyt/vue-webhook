c9b07f2dc69a21a3e6ed936a7b2d500810c2e113
霍怡童 <huoyt@csdn.net>
2020-09-01 07:56:56 +0800

    dev: dev

--- webhook.js
+++ webhook.js
@@ -1,9 +1,9 @@
 let http = require('http');
-http.createServer(function(req, res) {
+let server = http.createServer(function(req, res) {
     if(req.method == 'POST' && req.url == '/webhook') {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({ok: true}));
-        
+
     } else {
         res.end('Not Found');
     }
