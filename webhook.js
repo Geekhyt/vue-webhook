3b33b3276708f51c78ef319e40a9354e1f4068b7
霍怡童 <huoyt@csdn.net>
2020-09-01 08:37:59 +0800

    dev: dev

--- webhook.js
+++ webhook.js
@@ -1,7 +1,24 @@
 let http = require('http');
+let crypto = require('crypto');
+let SECRET = '123456';
+function sign(body) {
+    return `sha1=` + crypto.createHmac('sha1', SECRET).update(body).digest('hex');
+}
 let server = http.createServer(function(req, res) {
     if(req.method == 'POST' && req.url == '/webhook') {
-        console.log(req.method, req.url);
+        let buffers = [];
+        req.on('data', function(buffer) {
+            buffers.push(buffer);
+        });
+        req.on('end', function(buffer) {
+            let body = Buffer.concat(buffers);
+            let event = req.header['x-github-event']; // event = push
+            // github 请求来的时候，要传递请求体body、signature，你需要验签
+            let signature = req.headers['x-hub-signature'];
+            if(signature !== sign(body)) {
+                res.end('Not Allowed')
+            }
+        })
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({ok: true}));
     } else {
